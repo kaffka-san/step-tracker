8b1b8c48260b7ce995f39b1020e15d8526493ee9
Anastasia Lenina <anastasia.lenina@applifting.cz>
2024-05-05 13:32:22 +0200

    initial UI

--- StepTracker/StepTracker/ContentView.swift
+++ StepTracker/StepTracker/ContentView.swift
@@ -9,16 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationStack {
+            ScrollView {
+                steps()
+                    .padding()
+                averages()
+                    .padding()
+            }
+            .navigationTitle("Dashboard")
+        }
+    }
+
+    @ViewBuilder
+    func steps() -> some View {
         VStack {
-            // test image
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Label("Steps", systemImage: "figure.walk")
+                        .font(.title3.bold())
+                        .foregroundStyle(.pink)
+                    Text("Avg: 10K Steps")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundStyle(.secondary)
+            }
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.secondary)
+                .frame(height: 150)
         }
         .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color(.secondarySystemBackground))
+
+        }
+    }
+
+    @ViewBuilder
+    func averages() -> some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Label("Averages", systemImage: "calendar")
+                        .font(.title3.bold())
+                        .foregroundStyle(.pink)
+                    Text("Last 28 days")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundStyle(.secondary)
+            }
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.secondary)
+                .frame(height: 240)
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color(.secondarySystemBackground))
+
+        }
     }
 }
+
 
 #Preview {
     ContentView()
