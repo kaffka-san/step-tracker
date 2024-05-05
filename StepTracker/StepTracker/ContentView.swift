709ab32ef5f609e8c34b1f4ae99c11d15fe8e4f3
Kaffka <127501232+kaffka-san@users.noreply.github.com>
2024-05-05 12:23:19 +0200

    Initial commit

---
+++ StepTracker/StepTracker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  StepTracker
+//
+//  Created by Anastasia Lenina on 05.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            // test image
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Anastasia Lenina on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
